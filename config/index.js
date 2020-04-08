f7e65fa67b95832e3ed46bde1ccc5341d7740877
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-04-08 12:47:26 +0100

    navbar and theme created

---
+++ config/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+    title: `Florin Dumitru | Front End Developer`,
+    siteUrl: "https://www.florin-dumitru.co.uk",
+    description: `I am a highly motivated Front End Developer based in London, United Kingdom, who is passionate about creating rich user interfaces and interactive web applications using a wide range of JavaScript technologies and libraries.
+      I strongly believe in the importance of writing clean, modular and loosely coupled code that is easy to document, maintain and reuse.
+      I have experience working with HTML, CSS, JavaScript, React, Redux, TypeScript, GraphQL, Apollo, Electron, Jest, Webpack.`,
+    keywords:
+      "Florin Dumitru, Florin, Dumitru, DTFsquad, flololo, front end developer, developer, software developer, web developer, javascript, London, Romania",
+    author: `Florin Dumitru`,
+    siteLanguage: "en_GB",
+    location: "London, UK",
+    email: "dumitruflorin19@yahoo.com",
+    github: "https://github.com/DTFsquad",
+    twitter: "@flololo",
+    socialMedia: [
+      {
+        name: "Github",
+        url: "https://github.com/fDTFsquad",
+      },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/florin-dumitru-a997941a/",
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/flololo",
+      },
+    ],
+  }

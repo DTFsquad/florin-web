962de4974791b3edb6c8eb4beb54b52993f5e3c4
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-04-13 10:32:16 +0100

    progress bar

--- src/components/progressBar/progressBar.js
+++ src/components/progressBar/progressBar.js
@@ -30,12 +30,12 @@ const ProgressBarFrame = styled.div`
   }
 `;
 
-export const ProgressBar = ({ description = "", width, ...props }) => {
+export const ProgressBar = ({ description = "", level, ...props }) => {
   return (
     <ProgressBarFrame
       role="presentation"
       aria-label="progress bar for skill"
-      width={width}
+      width={level}
       description={description}
       {...props}
     >

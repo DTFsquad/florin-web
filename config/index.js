f9a24bceea79aabd98f6ce1d66ff73f06fedf73b
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-04-13 21:23:39 +0100

    fix typo

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     {
       name: "Linkedin",
-      url: "https://www.linkedin.com/in/florin-dumitru-a997941a/,
+      url: "https://www.linkedin.com/in/florin-dumitru-a997941a/",
     },
     {
       name: "Twitter",

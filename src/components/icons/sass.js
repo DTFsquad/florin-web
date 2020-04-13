c893ac16458f7bf18904d48f44cfaf9f6ad04c4a
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-04-13 06:54:43 +0100

    fix typo in sass.js and header.js

--- src/components/icons/sass.js
+++ src/components/icons/sass.js
@@ -6,7 +6,7 @@ export default () => (
     viewBox="0 0 256 192"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xmlns="http://www.w3.org/1999/xlink"
     preserveAspectRatio="xMidYMid"
   >
     <title>Sass</title>

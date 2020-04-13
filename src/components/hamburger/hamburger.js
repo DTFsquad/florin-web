98678423066c28446ceb8b585dda48740950e5fb
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-04-13 12:00:13 +0100

    move sidedrawer to appear on the right

--- src/components/hamburger/hamburger.js
+++ src/components/hamburger/hamburger.js
@@ -8,7 +8,7 @@ const HamburgerButton = styled.button`
   display: none;
   @media screen and (max-width: 720px) {
     position: fixed;
-    z-index: 999;
+    z-index: 1500;
     top: 20px;
     right: 20px;
     outline: none;

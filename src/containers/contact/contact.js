90b47249b1a1385e0e732ff6ada39a110f805526
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-04-20 13:38:18 +0100

    reduce delay in contact section animation

--- src/containers/contact/contact.js
+++ src/containers/contact/contact.js
@@ -51,7 +51,7 @@ const Contact = () => {
         css={header}
         data-aos="fade-up"
         data-aos-duration="350"
-        data-aos-delay="500"
+        data-aos-delay="250"
       >
         Get in touch
       </h4>
@@ -59,7 +59,7 @@ const Contact = () => {
         css={subText}
         data-aos="fade-up"
         data-aos-duration="350"
-        data-aos-delay="500"
+        data-aos-delay="250"
       >
         I am not available to any freelance work at the moment, but I am always
         happy to consider new ideas or collaborations, or even just to have a
@@ -69,7 +69,7 @@ const Contact = () => {
         href="mailto:dumitruflorin19@yahoo.com?subject=Get%20in%20Touch"
         data-aos="fade-up"
         data-aos-duration="350"
-        data-aos-delay="500"
+        data-aos-delay="250"
       >
         Contact me
       </LinkButton>
